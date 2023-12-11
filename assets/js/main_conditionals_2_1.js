7b063989801c369a71b10fe8102a4ea092acec2e
JohannaHaer <johanna.haerdter@gmail.com>
2023-12-11 17:16:07 +0100

    Aufgabe conditionals 2.1 hinzugefügt

--- assets/js/main_conditionals_2_1.js
+++ assets/js/main_conditionals_2_1.js
@@ -1,14 +1,3 @@
-// let sizeJohn = 170;
-// let sizeMeike = 168;
-// let ageJohn = 22;
-// let ageMeike = 34;
-
-// function math(age, size) {
-//     let score = age * 5 + size
-// }
-
-// math(ageJohn, sizeJohn)
-
 let scoreJohn = 0;
 let scoreMeike = 0;
 
@@ -43,3 +32,33 @@ function comparison() {
 comparison()
 
 
+// const ageJohn = 22
+// const ageMeike = 34
+// const sizeJohn = 170
+// const sizeMeike = 168
+// const nameJohn = "John"
+// const nameMeike = "Meike"
+// let scoreJohn = 0
+// let scoreMeike = 0
+
+// function calcScore(age, size, name){
+//     if(name == "John")
+//         scoreJohn = age * 5 + size
+//     else if(name == "Meike")
+//         scoreMeike = age * 5 + size
+// }
+
+// function compare(){
+//     calcScore(ageJohn, sizeJohn, nameJohn)
+//     calcScore(ageMeike, sizeMeike, nameMeike)
+//     if(scoreMeike > scoreJohn){
+//         console.log("Meike wins")
+//     }
+//     else if(scoreJohn > scoreMeike){
+//         console.log("John wins")
+//     } else{
+//         console.log("Unentschieden")
+//     }
+// }
+
+// compare()
